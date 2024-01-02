84b081c568d47033392c567dce305666464366f7
Pratik Bhatt <pratik.b@crestinfosystems.com>
2024-01-02 22:14:09 +0530

    Git Allow

--- src/authFiles/fbaseconfig.js
+++ src/authFiles/fbaseconfig.js
@@ -2,21 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseLiveConfig = {
-  apiKey: "AIzaSyBP5ilhGBqT0q0RnhE1eR6jMIjNhABQi70",
-  authDomain: "cricket-punter-line-dev.firebaseapp.com",
-  projectId: "cricket-punter-line-dev",
-  storageBucket: "cricket-punter-line-dev.appspot.com",
-  messagingSenderId: "2266165087",
-  appId: "1:2266165087:web:fd6d4ac5906e62c3ed90ea",
+  apiKey: "AIzaSyAYxWt-eySP188-AZQGar7A4Yy29_CpVUc",
+  authDomain: "cricket-pandit-ji-astro.firebaseapp.com",
+  projectId: "cricket-pandit-ji-astro",
+  storageBucket: "cricket-pandit-ji-astro.appspot.com",
+  messagingSenderId: "331394177719",
+  appId: "1:331394177719:web:46c1971a38517986a6cb32",
 };
 
 const firebaseDevConfig = {
-  apiKey: "AIzaSyBP5ilhGBqT0q0RnhE1eR6jMIjNhABQi70",
-  authDomain: "cricket-punter-line-dev.firebaseapp.com",
-  projectId: "cricket-punter-line-dev",
-  storageBucket: "cricket-punter-line-dev.appspot.com",
-  messagingSenderId: "2266165087",
-  appId: "1:2266165087:web:fd6d4ac5906e62c3ed90ea",
+  apiKey: "AIzaSyAYxWt-eySP188-AZQGar7A4Yy29_CpVUc",
+  authDomain: "cricket-pandit-ji-astro.firebaseapp.com",
+  projectId: "cricket-pandit-ji-astro",
+  storageBucket: "cricket-pandit-ji-astro.appspot.com",
+  messagingSenderId: "331394177719",
+  appId: "1:331394177719:web:46c1971a38517986a6cb32",
 };
 
 const app = initializeApp(process.env.REACT_APP_DEV === 'true' ? firebaseLiveConfig : firebaseDevConfig);
